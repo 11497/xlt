3818521a874f800064dc0502f58fb511cad12bbc
lvmr <lvonnb@163.com>
2026-07-10 15:20:14 +0800

    user注释与接口返回格式

--- router/user_router.py
+++ router/user_router.py
@@ -26,7 +26,7 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
     return payload
 
 @router.post("/regisster")
-async def register(user: User):
+async def register(user: User) -> Result:
     """用户注册"""
     result = Result()
     if len(user.password) < 6:
@@ -42,6 +42,7 @@ async def register(user: User):
 
 @router.post("/auth")
 async def auth(form_data: OAuth2PasswordRequestForm = Depends()):
+    """Swagger 认证接口"""
     login_user = UserCRUD.get_by_username(form_data.username)
     if login_user.password != form_data.password:
         return HTTPException(status_code=401, detail="密码错误")
@@ -50,7 +51,8 @@ async def auth(form_data: OAuth2PasswordRequestForm = Depends()):
     return {"access_token": token, "token_type": "bearer"}
 
 @router.post("/login")
-async def login(username: str, password: str):
+async def login(username: str, password: str) -> Result:
+    """用户登录"""
     result = Result()
     login_user = UserCRUD.get_by_username(username)
     if not login_user:
@@ -62,7 +64,7 @@ async def login(username: str, password: str):
     return result.success(msg="登录成功", data=access_token)
 
 @router.get("/all")
-async def get_all_user(token: str = Depends(oauth2_scheme)):
+async def get_all_user(token: str = Depends(oauth2_scheme)) -> Result:
     result = Result()
     current_user = get_current_user(token)
     user = UserCRUD.get_by_id(current_user["user_id"])
@@ -73,7 +75,8 @@ async def get_all_user(token: str = Depends(oauth2_scheme)):
     return result.success(msg="查询成功", data=[user for user in users])
 
 @router.get("")
-async def get_user(token: str = Depends(oauth2_scheme)):
+async def get_user(token: str = Depends(oauth2_scheme)) -> Result:
+    """查询当前用户信息"""
     result = Result()
     current_user = get_current_user(token)
     user = UserCRUD.get_by_id(current_user["user_id"])
@@ -83,15 +86,20 @@ async def get_user(token: str = Depends(oauth2_scheme)):
     return result.success(msg="查询成功", data=user)
 
 @router.put("/username")
-async def update_username(id: int, username: str, token: str = Depends(oauth2_scheme)):
+async def update_username(id: int, username: str, token: str = Depends(oauth2_scheme)) -> Result:
+    """管理员更新用户名"""
     result = Result()
     current_user = get_current_user(token)
     admin_user = UserCRUD.get_by_id(current_user["user_id"])
+
     if admin_user.is_admin == 0 or admin_user is None:
         return result.error(msg="您没有权限更新用户名")
+
     user_exist = UserCRUD.get_by_username(username)
     if user_exist:
         return result.error(msg="用户名已存在")
+    if len(username) < 4 or len(username) > 15:
+        return result.error(msg="用户名长度必须在4到15位之间")
 
     update_result = UserCRUD.update_username(id, username)
     if not update_result:
@@ -100,7 +108,8 @@ async def update_username(id: int, username: str, token: str = Depends(oauth2_sc
     return result.success(msg="更新成功", data=updated_user)
 
 @router.delete("/{id}")
-async def delete_user(id: int, token: str = Depends(oauth2_scheme)):
+async def delete_user(id: int, token: str = Depends(oauth2_scheme)) -> Result:
+    """管理员删除用户"""
     result = Result()
     current_user = get_current_user(token)
     admin_user = UserCRUD.get_by_id(current_user["user_id"])
@@ -112,26 +121,27 @@ async def delete_user(id: int, token: str = Depends(oauth2_scheme)):
     return result.success(msg="删除成功")
 
 @router.post("/password")
-async def update_password(old_password: str, new_password: str, token: str = Depends(oauth2_scheme)):
+async def update_password(old_password: str, new_password: str, token: str = Depends(oauth2_scheme)) -> Result:
+    """用户更新密码"""
     result = Result()
     current_user = get_current_user(token)
     user = UserCRUD.get_by_id(current_user["user_id"])
     if user is None:
         return result.error(msg="用户不存在")
 
-    if len(new_password) < 6:
-        return {"code": 1, "msg": "密码长度不能小于6位", "data": None}
+    if len(new_password) < 6 or len(old_password) < 6:
+        return result.error(msg="密码长度不能小于6位")
     if old_password == new_password:
-        return {"code": 1, "msg": "新密码不能与旧密码相同", "data": None}
+        return result.error(msg="新密码不能与旧密码相同")
 
     user_exist = UserCRUD.get_by_id(user.id)
     if not user_exist:
-        return {"code": 1, "msg": "用户不存在", "data": None}
+        return result.error(msg="用户不存在")
 
     # 密码校验
     if user_exist.password != old_password:
-        return {"code": 1, "msg": "旧密码错误", "data": None}
+        return result.error(msg="旧密码错误")
     res = UserCRUD.update_password(user.id, new_password)
     if not res:
-        return {"code": 1, "msg": "更新密码失败", "data": None}
-    return {"code": 0, "msg": "更新成功", "data": None}
+        return result.error(msg="更新密码失败")
+    return result.success(msg="更新成功")
