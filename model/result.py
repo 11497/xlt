463f3431f98f41e34f887cff09203197d79d8773
lvmr <lvonnb@163.com>
2026-07-10 15:14:10 +0800

    接口返回结构标准化

---
+++ model/result.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+
+class Result:
+    """接口返回模型"""
+    code: int
+    msg: str
+    data: Any
+
+    def __init__(self, code: int = 0, msg: str = "", data: Any = None):
+        self.code = code
+        self.msg = msg
+        self.data = data
+
+    def success(self, msg: str = "操作成功", data: Any = None):
+        self.code = 0
+        self.msg = msg
+        self.data = data
+        return self.json()
+
+    def error(self, msg: str = "操作失败", data: Any = None):
+        self.code = 1
+        self.msg = msg
+        self.data = data
+        return self.json()
+
+    def json(self):
+        return {"code": self.code, "msg": self.msg, "data": self.data}
