67a6f378c6d24e0ed06e7bdc31a888697660e917
lvmr <lvonnb@163.com>
2026-07-11 11:09:36 +0800

    user_router使用user_auth，并抽取出用户名、密码校验函数

--- router/user_router.py
+++ router/user_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from authentication.authentication import get_current_user, oauth2_scheme
+from authentication.user_auth import require_admin, require_current_user
 from config.jwt_config import JWT_CONFIG
 from crud.user_crud import UserCRUD
 from model.result import Result
@@ -12,17 +13,40 @@ router = APIRouter(prefix="/api/user", tags=["user"])
 jwt_util = JwtUtil(secret_key=JWT_CONFIG["secret_key"], algorithm=JWT_CONFIG["algorithm"],
                    access_token_expire_minutes=JWT_CONFIG["access_token_expire_minutes"])
 
+def valid_username(username: str):
+    """验证用户名是否符合要求"""
+    if len(username) < 4 or len(username) > 15:
+        return "用户名长度必须在4到15位之间"
+    user = UserCRUD.get_by_username(username)
+    if user:
+        return "用户名已存在"
+    return None
+
+def valid_password(new_password: str, old_password: str = "", user: User = None):
+    """
+    验证密码是否符合要求
+    在传入old_password时必须同时传入user
+    """
+    if old_password != "":
+        if old_password == new_password:
+            return "旧密码不能与新密码相同"
+        if user.password != old_password:
+            return "旧密码错误"
+    if len(new_password) < 6 or len(new_password) > 20:
+        return "密码长度必须在6到20位之间"
+    return None
+
 @router.post("/register")
 async def register(user: User):
     """用户注册"""
     result = Result()
-    if len(user.password) < 6:
-        return result.error(msg="密码长度不能小于6位")
-    if len(user.username) < 4 or len(user.username) > 15:
-        return result.error(msg="用户名长度必须在4到15位之间")
-    user_exist = UserCRUD.get_by_username(user.username)
-    if user_exist:
-        return result.error(msg="用户名已存在")
+    password_result = valid_password(user.password)
+    if password_result is not None:
+        return result.error(msg=password_result)
+
+    username_result = valid_username(user.username)
+    if username_result is not None:
+        return result.error(msg=username_result)
 
     UserCRUD.create(user)
     return result.success(msg="注册成功")
@@ -41,36 +65,25 @@ async def login(username: str, password: str):
     return result.success(msg="登录成功", data=access_token)
 
 @router.get("/all")
-async def get_all_user(token: str = Depends(oauth2_scheme)):
+async def get_all_user(admin: User = Depends(require_admin)):
     result = Result()
-    current_user = get_current_user(token)
-    user = UserCRUD.get_by_id(current_user["user_id"])
-    if user.is_admin == 0 or user is None:
-        return result.error(msg="您没有权限查询所有用户")
 
     users = UserCRUD.get_all()
     return result.success(msg="查询成功", data=[user for user in users])
 
 @router.get("")
-async def get_user(token: str = Depends(oauth2_scheme)):
+async def get_user(user: User = Depends(require_current_user())):
     """查询当前用户信息"""
     result = Result()
-    current_user = get_current_user(token)
-    user = UserCRUD.get_by_id(current_user["user_id"])
 
     # 返回去掉 password 字段的用户信息
     user.password = ""
     return result.success(msg="查询成功", data=user)
 
 @router.put("/username")
-async def update_username(id: int, username: str, token: str = Depends(oauth2_scheme)):
+async def update_username(id: int, username: str, admin: User = Depends(require_admin)):
     """管理员更新用户名"""
     result = Result()
-    current_user = get_current_user(token)
-    admin_user = UserCRUD.get_by_id(current_user["user_id"])
-
-    if admin_user.is_admin == 0 or admin_user is None:
-        return result.error(msg="您没有权限更新用户名")
 
     user_exist = UserCRUD.get_by_username(username)
     if user_exist:
@@ -85,39 +98,27 @@ async def update_username(id: int, username: str, token: str = Depends(oauth2_sc
     return result.success(msg="更新成功", data=updated_user)
 
 @router.delete("/{id}")
-async def delete_user(id: int, token: str = Depends(oauth2_scheme)):
+async def delete_user(id: int, admin: str = Depends(require_admin())):
     """管理员删除用户"""
     result = Result()
-    current_user = get_current_user(token)
-    admin_user = UserCRUD.get_by_id(current_user["user_id"])
-    if admin_user.is_admin == 0 or admin_user is None:
-        return result.error(msg="您没有权限删除用户")
+
     delete_result = UserCRUD.delete(id)
     if not delete_result:
         return result.error(msg="用户不存在")
     return result.success(msg="删除成功")
 
 @router.post("/password")
-async def update_password(old_password: str, new_password: str, token: str = Depends(oauth2_scheme)):
+async def update_password(old_password: str, new_password: str,
+                          user: User = Depends(require_current_user())):
     """用户更新密码"""
     result = Result()
-    current_user = get_current_user(token)
-    user = UserCRUD.get_by_id(current_user["user_id"])
+
     if user is None:
         return result.error(msg="用户不存在")
+    password_result = valid_password(new_password, old_password, user)
+    if password_result is not None:
+        return result.error(msg=password_result)
 
-    if len(new_password) < 6 or len(old_password) < 6:
-        return result.error(msg="密码长度不能小于6位")
-    if old_password == new_password:
-        return result.error(msg="新密码不能与旧密码相同")
-
-    user_exist = UserCRUD.get_by_id(user.id)
-    if not user_exist:
-        return result.error(msg="用户不存在")
-
-    # 密码校验
-    if user_exist.password != old_password:
-        return result.error(msg="旧密码错误")
     res = UserCRUD.update_password(user.id, new_password)
     if not res:
         return result.error(msg="更新密码失败")
@@ -125,13 +126,9 @@ async def update_password(old_password: str, new_password: str, token: str = Dep
 
 
 @router.put("/admin-status")
-async def set_user_admin_status(id: int, is_admin: int, token: str = Depends(oauth2_scheme)):
+async def set_user_admin_status(id: int, is_admin: int, admin: User = Depends(require_admin)):
     """管理员设置用户权限"""
     result = Result()
-    current_user = get_current_user(token)
-    admin_user = UserCRUD.get_by_id(current_user["user_id"])
-    if admin_user.is_admin == 0 or admin_user is None:
-        return result.error(msg="您没有权限设置用户权限")
 
     # 验证目标用户是否存在
     target_user = UserCRUD.get_by_id(id)
@@ -143,7 +140,7 @@ async def set_user_admin_status(id: int, is_admin: int, token: str = Depends(oau
         return result.error(msg="管理员状态值无效，应为 0（普通用户）或 1（管理员）")
 
     # 防止管理员取消自己的管理员权限
-    if admin_user.id == id and is_admin == 0:
+    if admin.id == id and is_admin == 0:
         return result.error(msg="不能取消自己的管理员权限")
     update_result = UserCRUD.set_user_admin_status(id, is_admin)
     if not update_result:
@@ -151,4 +148,3 @@ async def set_user_admin_status(id: int, is_admin: int, token: str = Depends(oau
 
     updated_user = UserCRUD.get_by_id(id)
     return result.success(msg="用户权限更新成功", data=updated_user)
-
