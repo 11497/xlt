ae7c5489de7a13ba6297c5b51da59a7e1bd0b574
lvmr <lvonnb@163.com>
2026-07-11 11:16:34 +0800

    user_router使用user_auth

--- router/role_router.py
+++ router/role_router.py
@@ -1,26 +1,17 @@
 from fastapi import APIRouter, Depends
 
-from authentication.authentication import get_current_user, oauth2_scheme
-from config.jwt_config import JWT_CONFIG
+from authentication.user_auth import require_admin
 from crud.role_crud import RoleCRUD
-from crud.user_crud import UserCRUD
 from model.result import Result
 from model.role_model import Role
-from util.jwt_util import JwtUtil
+from model.user_model import User
 
 router = APIRouter(prefix="/api/role", tags=["role"])
 
-jwt_util = JwtUtil(secret_key=JWT_CONFIG["secret_key"], algorithm=JWT_CONFIG["algorithm"],
-                   access_token_expire_minutes=JWT_CONFIG["access_token_expire_minutes"])
-
 @router.post("")
-async def create_role(role: Role, token: str = Depends(oauth2_scheme)):
+async def create_role(role: Role, admin: User = Depends(require_admin())):
     """创建角色"""
     result = Result()
-    current_user = get_current_user(token)
-    user = UserCRUD.get_by_id(current_user["user_id"])
-    if user.is_admin == 0 or user is None:
-        return result.error(msg="您没有权限创建角色")
 
     role_id = RoleCRUD.create(role)
     if role_id is None:
@@ -28,25 +19,17 @@ async def create_role(role: Role, token: str = Depends(oauth2_scheme)):
     return result.success(msg="创建角色成功")
 
 @router.get("/all")
-async def get_all_role(token: str = Depends(oauth2_scheme)):
+async def get_all_role(admin: User = Depends(require_admin())):
     """查询所有角色"""
     result = Result()
-    current_user = get_current_user(token)
-    user = UserCRUD.get_by_id(current_user["user_id"])
-    if user.is_admin == 0 or user is None:
-        return result.error(msg="您没有权限查询所有角色")
 
     roles = RoleCRUD.get_all()
     return result.success(msg="查询所有角色成功", data=roles)
 
 @router.get("/{role_name}")
-async def get_by_name(role_name: str, token: str = Depends(oauth2_scheme)):
+async def get_by_name(role_name: str, admin: User = Depends(require_admin())):
     """根据角色名查询角色"""
     result = Result()
-    current_user = get_current_user(token)
-    user = UserCRUD.get_by_id(current_user["user_id"])
-    if user.is_admin == 0 or user is None:
-        return result.error(msg="您没有权限查询角色")
 
     role = RoleCRUD.get_by_name(role_name)
     if role is None:
@@ -54,13 +37,9 @@ async def get_by_name(role_name: str, token: str = Depends(oauth2_scheme)):
     return result.success(msg="查询角色成功", data=role)
 
 @router.put("")
-async def update(role: Role, token: str = Depends(oauth2_scheme)):
+async def update(role: Role, admin: User = Depends(require_admin())):
     """更新角色名"""
     result = Result()
-    current_user = get_current_user(token)
-    user = UserCRUD.get_by_id(current_user["user_id"])
-    if user.is_admin == 0 or user is None:
-        return result.error(msg="您没有权限更新角色名")
 
     update_result = RoleCRUD.update_name(role.id, role.name)
     if not update_result:
@@ -68,13 +47,9 @@ async def update(role: Role, token: str = Depends(oauth2_scheme)):
     return result.success(msg="更新角色名成功")
 
 @router.delete("")
-async def delete(id: int, token: str = Depends(oauth2_scheme)):
+async def delete(id: int, admin: User = Depends(require_admin())):
     """删除角色"""
     result = Result()
-    current_user = get_current_user(token)
-    user = UserCRUD.get_by_id(current_user["user_id"])
-    if user.is_admin == 0 or user is None:
-        return result.error(msg="您没有权限删除角色")
 
     delete_result = RoleCRUD.delete(id)
     if not delete_result:
