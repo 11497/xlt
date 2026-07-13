7159f0dc7abb1cfc62a62afce5020e149523d0b8
lvmr <lvonnb@163.com>
2026-07-13 15:09:33 +0800

    document相关文件创建

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from authentication import authentication
 from router import user_router, role_router, role_user_router, knowledge_base_router, role_knowledge_base_router, \
-    session_router, announcement_router, announcement_attachment_router
+    session_router, announcement_router, announcement_attachment_router, document_router
 
 app = FastAPI()
 
@@ -20,3 +20,4 @@ app.include_router(role_knowledge_base_router.router)
 app.include_router(session_router.router)
 app.include_router(announcement_router.router)
 app.include_router(announcement_attachment_router.router)
+app.include_router(document_router.router)
---
+++ router/document_router.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/api/document", tags=["document"])
