d5d602fd77efdcc4b2bad6066f1c46e8dbc9bfba
lvmr <lvonnb@163.com>
2026-07-16 16:36:19 +0800

    前端路由配置

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,47 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import LoginView from "@/views/LoginView.vue";
 import ChatView from "@/views/ChatView.vue";
+import AdminLayoutView from "@/views/admin/AdminLayoutView.vue";
+import UserLayoutView from "@/views/user/UserLayoutView.vue";
+import UserMyView from "@/views/user/UserMyView.vue";
+import AdminMyView from "@/views/admin/AdminMyView.vue";
+import AdminAnnouncementView from "@/views/admin/AdminAnnouncementView.vue";
+import AdminSessionView from "@/views/admin/AdminSessionView.vue";
+import AdminKnowledgeBaseView from "@/views/admin/AdminKnowledgeBaseView.vue";
+import AdminUserView from "@/views/admin/AdminUserView.vue";
+import AdminRoleView from "@/views/admin/AdminRoleView.vue";
 
 const routes = [
     {path: '/', name: 'login', component: LoginView},
     {path: '/login', name: 'loginView', component: LoginView},
-    {path: "/chat", name:'chat' , component: ChatView}
+    {path: "/chat", name: 'chat', component: ChatView},
+    {
+        path: "/admin",
+        name: "admin",
+        component: AdminLayoutView,
+        redirect: "/admin/my",
+        children: [
+            {path: "my", name: "admin-my", component: AdminMyView},
+            {path: "announcement", name: "admin-announcement", component: AdminAnnouncementView},
+            {path: "knowledgeBase", name: "admin-knowledgeBase", component: AdminKnowledgeBaseView},
+            {path: "role", name: "admin-role", component: AdminRoleView},
+            {path: "session", name: "admin-session", component: AdminSessionView},
+            {path: "user", name: "admin-user", component: AdminUserView}
+        ]
+    },
+    {
+        path: "/user",
+        name: "user",
+        component: UserLayoutView,
+        redirect: "/user/my",
+        children: [
+            {path: "my", name: "user-my", component: UserMyView},
+            {path: "announcement", name: "admin-announcement", component: AdminAnnouncementView},
+            {path: "knowledgeBase", name: "admin-knowledgeBase", component: AdminKnowledgeBaseView},
+            {path: "role", name: "admin-role", component: AdminRoleView},
+            {path: "session", name: "admin-session", component: AdminSessionView}
+        ]
+    }
 ]
 
 const router = createRouter({
