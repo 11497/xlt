497819e3743a22a9f1eacff2c201540e30e85b70
lvmr <lvonnb@163.com>
2026-07-17 14:53:57 +0800

    修改前端用户请求

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -10,16 +10,16 @@ export const userInfo = () => request.get("/user");
 export const userRegister = (user) => request.post("/user/register", user);
 
 // 分页查询所有用户信息（管理员）
-export const getAllUsers = (params) => request.get("/user/all", { params });
+export const getAllUsers = (params) => request.get("/user/all", params);
 
 // 管理员更新用户名
-export const updateUsername = (data) => request.put("/user/username", null, { params: data });
+export const updateUsername = (data) => request.put("/user/username", data);
 
 // 管理员删除用户
 export const deleteUser = (id) => request.delete(`/user/${id}`);
 
 // 用户更新密码
-export const updatePassword = (data) => request.post("/user/password", null, { params: data });
+export const updatePassword = (data) => request.put("/user/password", data);
 
 // 管理员设置用户权限
-export const setAdminStatus = (data) => request.put("/user/admin-status", null, { params: data });
+export const setAdminStatus = (data) => request.put("/user/admin-status", data);
