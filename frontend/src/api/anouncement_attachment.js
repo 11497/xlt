0cd04892d592fb6007be06323055e6bd85320258
lvmr <lvonnb@163.com>
2026-07-17 11:03:00 +0800

    修改公告附件的请求至与后端匹配

--- frontend/src/api/anouncement_attachment.js
+++ frontend/src/api/anouncement_attachment.js
@@ -6,14 +6,16 @@ export const uploadAnnouncementAttachment = (formData) =>
     headers: { "Content-Type": "multipart/form-data" },
   });
 
-// 根据ID下载公告附件
+// 下载公告附件
 export const downloadAnnouncementAttachment = (id) =>
-  request.get(`/announcement-attachment/download/${id}`, { responseType: "blob" });
+  request.get(`/announcement-attachment/download/${id}`, {
+    responseType: "blob",
+  });
 
-// 根据ID删除公告附件
+// 删除公告附件
 export const deleteAnnouncementAttachment = (id) =>
   request.delete(`/announcement-attachment/${id}`);
 
-// 获取指定公告的所有附件列表
-export const getAttachmentsByAnnouncementId = (announcementId) =>
+// 获取公告的所有附件列表
+export const getAnnouncementAttachments = (announcementId) =>
   request.get(`/announcement-attachment/announcement/${announcementId}`);
