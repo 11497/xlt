fa3a512ab38b36474d67ec7c4df8b25080a28c2b
lvmr <lvonnb@163.com>
2026-07-17 13:29:28 +0800

    前端消息请求

--- frontend/src/api/message.js
+++ frontend/src/api/message.js
@@ -5,3 +5,7 @@ export const messageBySessionId = (sessionId) => request.get(`/message/session/$
 export const chat = (message) => request.post("/message/chat", message)
 
 export const deleteMessagesAfter = (session_id, message_id) => request.delete(`/message/after?session_id=${session_id}&message_id=${message_id}`)
+
+export const getMessageById = (messageId) => request.get(`/message/${messageId}`);
+
+export const deleteMessagesBySessionId = (sessionId) => request.delete(`/message/session/${sessionId}`);
