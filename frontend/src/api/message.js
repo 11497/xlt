04d948a58dd49ab5877c5a0dad99d758d10c5b45
lvmr <lvonnb@163.com>
2026-07-17 13:39:01 +0800

    前端消息请求添加注释

--- frontend/src/api/message.js
+++ frontend/src/api/message.js
@@ -1,11 +1,16 @@
 import request from "@/utils/request.js";
 
+// 通过会话ID查询所有消息
 export const messageBySessionId = (sessionId) => request.get(`/message/session/${sessionId}`)
 
+// 发送消息
 export const chat = (message) => request.post("/message/chat", message)
 
+// 删除指定消息ID及之后的所有消息
 export const deleteMessagesAfter = (session_id, message_id) => request.delete(`/message/after?session_id=${session_id}&message_id=${message_id}`)
 
+// 查询消息详情
 export const getMessageById = (messageId) => request.get(`/message/${messageId}`);
 
+// 删除指定会话ID下的所有消息
 export const deleteMessagesBySessionId = (sessionId) => request.delete(`/message/session/${sessionId}`);
