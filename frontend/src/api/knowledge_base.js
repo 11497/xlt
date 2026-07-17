7ea5aeb04942af97839385db622a9c0156bed78f
lvmr <lvonnb@163.com>
2026-07-17 11:33:02 +0800

    前端知识库请求

---
+++ frontend/src/api/knowledge_base.js
@@ -0,0 +1,13 @@
+import request from "@/utils/request.js";
+
+// 创建知识库
+export const createKnowledgeBase = (knowledgeBase) => request.post("/knowledge_base", knowledgeBase)
+
+// 分页查询所有知识库
+export const getAllKnowledgeBases = (page, pageSize) => request.get(`/knowledge_base/all?page=${page}&page_size=${pageSize}`)
+
+// 更新知识库
+export const updateKnowledgeBase = (knowledgeBase) => request.put("/knowledge_base", knowledgeBase)
+
+// 删除知识库
+export const deleteKnowledgeBase = (id) => request.delete(`/knowledge_base?id=${id}`)
