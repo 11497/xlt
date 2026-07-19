18ade1081ab3b4eb03e06aa536086c7c9fd1756f
lvmr <lvonnb@163.com>
2026-07-19 10:55:24 +0800

    修改分页查看用户的请求

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -10,7 +10,8 @@ export const userInfo = () => request.get("/user");
 export const userRegister = (user) => request.post("/user/register", user);
 
 // 分页查询所有用户信息（管理员）
-export const getAllUsers = (params) => request.get("/user/all", params);
+export const getAllUsers = (page = 1, pageSize = 10) =>
+    request.get("/user/all", { params: { page, page_size: pageSize } });
 
 // 管理员更新用户名
 export const updateUsername = (data) => request.put("/user/username", data);
